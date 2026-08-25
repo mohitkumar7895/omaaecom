5168f51b926ef05637f8512f2a692f04a0f6a2dc
mohitkumar7895 <mohitporwal596@gmail.com>
2026-08-25 18:58:06 +0530

    rate cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
   title: "OMAA Company - Get Home appliances Expert services……..",
   description: "OMAA Company - Get Home appliances Expert services at your doorstep",
   icons: {
-    icon: '/favicon.jpeg',
-    shortcut: '/favicon.jpeg',
-    apple: '/favicon.jpeg',
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
   },
 };
 
